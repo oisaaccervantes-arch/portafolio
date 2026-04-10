b2c1d2a4621da859224677a82385bc3a6e287248
oisaaccervantes-arch <oisaaccervantes@gmail.com>
2026-04-10 10:50:11 -0700

    Add files via upload

---
+++ Portafolio/main.js
@@ -0,0 +1,9 @@
+console.log("Portafolio cargado correctamente");
+
+const boton = document.querySelector("#contactBtn");
+
+if (boton) {
+    boton.addEventListener("click", () => {
+        alert("Gracias por visitar mi portafolio");
+    });
+}
